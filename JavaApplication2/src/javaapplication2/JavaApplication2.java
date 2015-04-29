94d5c4b7bd4cd7a7a29f17b6d2d98a056e667365
unknown <poker15@A109-09.itlab.pmmk.pte.hu>
2015-04-29 18:00:03 +0200

    szia világ

--- JavaApplication2/src/javaapplication2/JavaApplication2.java
+++ JavaApplication2/src/javaapplication2/JavaApplication2.java
@@ -16,7 +16,7 @@ public class JavaApplication2 {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        System.out.println("Szia világ");
     }
     
 }
